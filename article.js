1aad599667830bf0e01ae4a4bb408b8131abec58
Jojo  Young <jojoyoung3@gmail.com>
2024-08-15 12:13:17 +0000

    Update article.js

--- article.js
+++ article.js
@@ -1,18 +1,34 @@
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const shareImg = document.getElementById('shareimg');
     const iconShareActive = document.getElementById('icon-share-active');
     const shareOpenContainer = document.querySelector('.share-open-container');
     const wholeImgContainer = document.querySelector('.wholeImgContainer');
-
+    
     shareOpenContainer.style.display = 'none';
 
     function toggleShare() {
-        if (shareOpenContainer.style.display === 'none') {
-            shareOpenContainer.style.display = 'flex';
-            wholeImgContainer.style.display = 'none';
-        }else if(wholeImgContainer.style.display === 'none'){
-            shareOpenContainer.style.display = 'none';
-            wholeImgContainer.style.display = 'flex';
+        if (window.innerWidth <= 1400) {
+            if (shareOpenContainer.style.display === 'none') {
+                shareOpenContainer.style.display = 'flex';
+                wholeImgContainer.style.display = 'none';
+            } else {
+                shareOpenContainer.style.display = 'none';
+                wholeImgContainer.style.display = 'flex';
+            }
+        } else {
+            if (shareOpenContainer.style.display === 'none') {
+                shareOpenContainer.style.display = 'flex';
+                shareImg.style.display = 'none'; 
+                wholeImgContainer.appendChild(iconShareActive);
+            } else {
+                shareOpenContainer.style.display = 'none';
+                shareImg.style.display = 'block'; 
+                if (iconShareActive.parentNode === wholeImgContainer){
+                    wholeImgContainer.removeChild(iconShareActive);
+                }
+            }
         }
     }
 
